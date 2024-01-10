7314290ae71eb80b9d2a31b541287f8600831ba0
grahamtaggart <graham.n.taggart.dut@gmail.com>
2024-01-10 12:45:25 -0700

    Finally working as intented

--- python/fasta_analysis.py
+++
@@ -1,7 +0,0 @@
-#! C:\Users\graha\ecoli-dna-pipeline-1\venv\Scripts\python.exe
-# fasta_analysis is used for visualization, GWAS, 
-from Bio import SeqIO, Entrez
-
-Entrez.email = 'graham.n'
-
-
---
+++ python/org_parse.py
@@ -0,0 +1,24 @@
+from Bio import Entrez, SeqIO
+
+# Trying my current strategy with another organism, seeing if that makes any difference or not, if not try different programming.
+Entrez.email = 'graham.n.taggart.dut@gmail.com'
+
+def search_and_fetch_org_sequence(organism):
+    # Step 1: Search the nucleotide database for HIV fasta files
+    search_term = str(organism)
+    handle = Entrez.esearch(db="nucleotide", term=search_term, retmax=1)
+    record_id = Entrez.read(handle)["IdList"][0]
+    handle.close()
+
+    # Step 2: Efetch a single record
+    handle = Entrez.efetch(db="nucleotide", id=record_id, rettype="fasta", retmode="text")
+    sequence_record = SeqIO.read(handle, "fasta")
+    handle.close()
+
+    # Step 3: Print the sequence or perform analysis
+    print("Record ID:", record_id)
+    print("Sequence Description:", sequence_record.description)
+    print("Sequence:")
+    print(sequence_record.seq[0:50] + '...')
+
+search_and_fetch_org_sequence('ecoli')
