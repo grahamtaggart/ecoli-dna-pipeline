42d796c23bb0bde606f99b6ef31dfb16856e7296
grahamtaggart <graham.n.taggart.dut@gmail.com>
2024-01-11 14:30:47 -0700

    change

--- python/org_parse.py
+++ python/org_parse.py
@@ -1,9 +1,9 @@
 from Bio import Entrez, SeqIO
-
+from collections import Counter
 # Trying my current strategy with another organism, seeing if that makes any difference or not, if not try different programming.
 Entrez.email = 'graham.n.taggart.dut@gmail.com'
 
-def search_and_fetch_org_sequence(organism):
+def search_and_fetch_org(organism):
     # Step 1: Search the nucleotide database for HIV fasta files
     search_term = str(organism)
     handle = Entrez.esearch(db="nucleotide", term=search_term, retmax=1)
@@ -19,9 +19,14 @@ def search_and_fetch_org_sequence(organism):
     print("Record ID:", record_id)
     print("Sequence Description:", sequence_record.description)
     print("Sequence:")
-    print(sequence_record.seq[0:50] + '...')
+    print(sequence_record.seq[0:50] + '...' + sequence_record.seq[-50:])
+    print('Sequence Length:', len(sequence_record.seq))
+    return sequence_record
 
-search_and_fetch_org_sequence('ecoli')
+# search_and_fetch_org('ecoli')
 
-def seq_analysis():
-    pass
+def seq_analysis(function):
+    print(Counter(function.seq))
+    
+
+seq_analysis(search_and_fetch_org('ecoli'))
